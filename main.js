4f84cec0a65bb8f1f0c2516237da3b624d4e46c7
Nikhil Rane <nikhilrane072@gmail.com>
2023-02-07 12:02:49 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let snd = new Audio('./assets/alarm-clock-short.mp3')
+
+function randomAlarm() {
+  let randNum = Math.ceil(Math.random() * 3600)
+  console.log(randNum)
+  if (randNum <= 30) {
+    document.innerHTML = 'Beeping rn!'
+    snd.play();
+  }
+}
+
+setInterval(randomAlarm, 1000)
